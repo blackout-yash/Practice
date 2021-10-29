a4c8fa34cb441a9b8ddcc866c95155e8af968114
Yash Kumar <yashkumarjmp2002@gmail.com>
2021-10-29 18:14:02 +0530

    Practice

---
+++ Dp/24_Palindromic_patitioning.cpp
@@ -0,0 +1,58 @@
+// Palindromic patitioning
+// Link - https://practice.geeksforgeeks.org/problems/palindromic-patitioning4845/1#
+
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+bool isPalidrome(string &str, int i, int j) {
+	string temp2 = str.substr(i, j - i + 1);
+	string temp1 = temp2;
+
+	reverse(temp2.begin(), temp2.end());
+	if (temp1 == temp2) return true;
+	return false;
+}
+
+
+int solve(string &str, int i, int j, vector <vector <int>> &dp) {
+	if (i >= j) return 0;
+	else if (dp[i][j] != -1) return dp[i][j];
+	else if (isPalidrome(str, i, j)) {
+		dp[i][j] = 0;
+		return 0;
+	}
+
+	int minimum = INT_MAX;
+	for (int k = i; k < j; k++) {
+		minimum = min(minimum, solve(str, i, k, dp) + solve(str, k + 1, j, dp) + 1);
+	}
+	return dp[i][j] = minimum;
+}
+
+
+// Top Down
+int palindromicPartition(string str) {
+	int n = str.size();
+	vector <vector <int>> dp(n, vector <int>(n, -1));
+	return solve(str, 0, n - 1, dp);
+}
+
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		string str;
+		cin >> str;
+
+		cout << palindromicPartition(str) << " ";
+	}
+
+	return 0;
+}
